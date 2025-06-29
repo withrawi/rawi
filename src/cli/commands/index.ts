81e8bf85601242905f18b4411de23f22c8f22333
MKAbuMattar <mohammad.khaled@outlook.com>
2025-06-30 00:29:36 +0300

    fix: remove unnecessary newline in index.ts exports

--- src/cli/commands/index.ts
+++ src/cli/commands/index.ts
@@ -3,5 +3,4 @@ export * from './ask.command.js';
 export * from './configure.command.js';
 export * from './history.command.js';
 export * from './info.command.js';
-
 export * from './provider.command.js';
